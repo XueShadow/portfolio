6a70c802988b2ea1f3ba3420d270e72409f0ae3a
Xue Shadow <Xueshadow@icloud.com>
2026-05-25 01:41:08 +0800

    Updates

--- server/assets/_tanstack-start-manifest_v-BThLuMph.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Xue/Downloads/ji-monsales-portfolio-main/ji-monsales-portfolio-main/src/routes/__root.tsx", children: ["/"], preloads: ["/portfolio/assets/index-B6C1ilg9.js"] }, "/": { filePath: "C:/Users/Xue/Downloads/ji-monsales-portfolio-main/ji-monsales-portfolio-main/src/routes/index.tsx", children: void 0, preloads: ["/portfolio/assets/index-Dc03_FJI.js"] } }, clientEntry: "/portfolio/assets/index-B6C1ilg9.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-DFQha--V.js";
-import "node:events";
-export {
-  w as default
-};
